11ddf60d5a34a28f09a60f53fac85c643e1e31ea
Jan Phillips <janpatekphillips@gmail.com>
2023-11-10 11:21:53 +0000

    print 0xffff when no key press and 0x0000 when 'c' pressed

---
+++ checkerboard.asm
@@ -0,0 +1,45 @@
+// when no key pressed print checkerboard in figure 1
+// when 'c' pressed print inverse checkerboard
+
+// while KBD != 'c'
+//	print(0xffff,0x0000 * 16)
+//	if KBD == 'c'
+//		print(0xffff,0x0000, * 16)
+
+
+(wait_for_c_press)
+
+// print default checkboard 
+// set 0x4000 to 1
+@SCREEN
+D=0
+M=!D
+
+// get input from KBD
+@KBD
+D=M
+
+// if 'c' not presed; if KBD - 67 != 0
+// go to top of program; re-print checkerboard
+@67
+M=A
+D=D-M
+
+@wait_for_c_press
+D;JNE
+
+// if 'c'pressed; if KBD - 67 == 0
+// print inverse keyboard
+@SCREEN
+D=M
+M=!D
+
+
+
+
+
+
+
+
+@wait_for_c_press
+0;JMP
