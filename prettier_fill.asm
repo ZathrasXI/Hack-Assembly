b36375120e747e1ba94bb346fa00c9393cd1954e
Jan Phillips <janpatekphillips@gmail.com>
2023-11-11 07:55:04 +0000

    fills screen with columns, when 'c' is pressed all words flip

--- prettier_fill.asm
+++ prettier_fill.asm
@@ -5,6 +5,17 @@
 // infinite loop checking for 'c' being pressed
 (check_for_c_press)
 
+//starting address
+@SCREEN
+D=A
+@current_address
+M=D
+A=D
+
+//@i
+M=0
+
+(get_word)
 // go to KBD check if input == 67 ('c')
 @KBD
 D=M
@@ -21,16 +32,33 @@ D;JEQ
 D=A
 
 
-//print board
 (print_board)
-@SCREEN
+// go to current address
+@current_address
+A=M
+//PRINT word
 M=D
 
+//*?*?
+//increment address
+@current_address
+M=M+1
+D=M
+
+//if final address reached, restart
+@24575
+D=D-A
+@check_for_c_press
+D;JEQ
 
 
+//set word for next iteration
+@21845
+D=A
 
-
-
+@get_word
+0;JMP
+//*?*?
 
 @check_for_c_press
 0;JMP
