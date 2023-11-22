4a04de37679e819393207d6ee990d6933d24aa10
Jan Phillips <janpatekphillips@gmail.com>
2023-11-22 18:03:59 +0000

    complete; checkerboard is printed to screen, and is inverted when 'c' key is press

--- prettier_fill.asm
+++ prettier_fill.asm
@@ -1,72 +1,84 @@
-//print checkerboard
-// when 'c' is pressed invert entire board
+(start)
+@i
+M=0
 
-
-// infinite loop checking for 'c' being pressed
-(check_for_c_press)
-
-//starting address
+//address variable
 @SCREEN
 D=A
 @current_address
 M=D
-A=D
 
-//@i
-M=0
-
-(get_word)
-// go to KBD check if input == 67 ('c')
-@KBD
-D=M
-@67
-M=A
-D=D-M
-
-// if KBD == 'c' flip binary word
-@flip_binary_word
-D;JEQ
-
-//else
 @21845
 D=A
-
-
-(print_board)
-// go to current address
-@current_address
-A=M
-//PRINT word
+@word
 M=D
 
-//*?*?
-//increment address
+//check for 'c' press
+@67
+D=A
+@KBD
+D=D-M
+//flip word if 'c'
+@flip_word
+D;JEQ
+
+//if not 'c', set D to word
+@word
+D=M
+
+
+(print_screen)
+@current_address
+A=M
+//print word
+M=D
+
+
+//update address
 @current_address
 M=M+1
 D=M
 
-//if final address reached, restart
+// if on final address of screen 0x5fff
+// check for key press
 @24575
 D=D-A
-@check_for_c_press
+@start
 D;JEQ
 
+//increment i
+@i
+M=M+1
+D=M
 
-//set word for next iteration
-@21845
-D=A
 
-@get_word
+
+//flip word if i == 32
+@32
+M=A
+D=D-M
+@reset_i
+D;JEQ
+
+//else
+@word
+D=M
+
+
+@print_screen
 0;JMP
-//*?*?
 
-@check_for_c_press
+
+(flip_word)
+@word
+M=!M
+D=M
+@print_screen
 0;JMP
 
-
-(flip_binary_word)
-@21845
-D=A
-D=!D
-@print_board
+(reset_i)
+@i
+M=0
+@flip_word
 0;JMP
+
